2bb71e1840b604d9863f3614d8dd2247a81e5c8c
Roman Koles <roman.koles@hotmail.com>
2020-08-21 14:37:19 -0400

    Update readme-steps.sh

--- readme-steps.sh
+++ readme-steps.sh
@@ -2,13 +2,13 @@
 docker build -t demo .
 
 # login to ECR (change for your region)
-$(aws ecr get-login --no-include-email --region eu-west-1 --profile aws-devops)
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 1234567890.dkr.ecr.us-east-2.amazonaws.com
 
 # tag image (change aws account number)
-docker tag demo:latest 1234567890.dkr.ecr.eu-west-1.amazonaws.com/demo:latest
+docker tag demo:latest 1234567890.dkr.ecr.us-east-2.amazonaws.com/demo:latest
 
 # push image
-docker push 1234567890.dkr.ecr.eu-west-1.amazonaws.com/demo:latest
+docker push 1234567890.dkr.ecr.us-east-2.amazonaws.com/demo:latest
 
 # pull image
-docker pull 1234567890.dkr.ecr.eu-west-1.amazonaws.com/demo:latest
+docker pull 1234567890.dkr.ecr.us-east-2.amazonaws.com/demo:latest
